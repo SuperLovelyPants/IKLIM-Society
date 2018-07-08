95c176471fe29e7ae0f810a0cccea037ff767c84
LHG <LHG@172.16.165.224>
2018-07-08 12:18:57 +0900

    work base model modified

---
+++ Society/src/iklim/society/model/base/rule/Rule.java
@@ -0,0 +1,53 @@
+package iklim.society.model.base.rule;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import iklim.society.model.base.AbstractBaseModel;
+import iklim.society.model.base.AbstractExecutable;
+import iklim.society.model.base.utility.Precondition;
+import iklim.society.model.instance.Work;
+import iklim.society.model.instance.argument.Argument;
+
+public class Rule extends AbstractExecutable{
+	private LinkedList<String>					effectWorks;
+	
+	private HashMap<String, Parameter>			params;
+	private LinkedList<RuleFactor>				factors;
+	private Evaluator							evaluator;
+	
+
+	public Rule() {
+		effectWorks = new LinkedList<String>();
+		params = new HashMap<String, Parameter>();
+		factors = new LinkedList<>();
+	}
+	
+	public void putParameter(Parameter param) {
+		String name = param.getName();
+		params.put(name, param);
+	}
+	
+	public void registerRuleFactor(RuleFactor r) {
+		factors.add(r);
+	}
+	
+	public void registerEffectWork(String workId) {
+		effectWorks.add(workId);
+		// TODO effectWork에 들어가는 target, worker가 현재 실행중인 Rule의 worker, 혹은 Target 맷칭되는 룰을 표현해야 함
+	}
+	
+	
+	public Collection<String> execute(String worker, String target, Argument[] args) {
+		for (RuleFactor r : factors) {
+			r.evaluate(params.get(r.getTargetParameter()));
+		}
+		
+		evaluator.evaluate(params);
+		
+		
+		return effectWorks;
+	}
+	
+}
